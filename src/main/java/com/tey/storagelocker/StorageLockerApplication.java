fe8567b0394773036ffd77fc0ea5acbd28393b87
Tainan Rodrigues <tainansilvasci@gmail.com>
2020-02-12 13:30:53 -0300

    first commit

---
+++ src/main/java/com/tey/storagelocker/StorageLockerApplication.java
@@ -0,0 +1,13 @@
+package com.tey.storagelocker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StorageLockerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StorageLockerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/tey/storagelocker/StorageLockerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tey.storagelocker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StorageLockerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
